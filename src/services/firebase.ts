abad3e2158b335888decb280f5ea58a266436fa2
Giovani <giovaniamaral2013@hotmail.com>
2022-04-03 19:20:04 -0300

    github imagem

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyB-mutU34buCXWbuNsMtwX52r988LfejAw",
+  authDomain: "letmeask-32ccb.firebaseapp.com",
+  databaseURL: "https://letmeask-32ccb-default-rtdb.firebaseio.com",
+  projectId: "letmeask-32ccb",
+  storageBucket: "letmeask-32ccb.appspot.com",
+  messagingSenderId: "1047687226474",
+  appId: "1:1047687226474:web:1f7ac25827768bb1e91613"
 };
 
 if (!firebase.apps.length) {
